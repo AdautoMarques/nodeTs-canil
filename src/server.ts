c7fa484bc56be46f9b1e6b5c60ad5def8b1f7a4c
Adauto Marques <adautomarques937@gmail.com>
2025-01-10 12:23:47 -0300

    Configurando as rotas do projeto

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+import mainRouters from "./routes/index";
 
 dotenv.config();
 
@@ -12,5 +13,11 @@ app.set("views", path.join(__dirname, "views"));
 app.engine("mustache", mustache());
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.use(mainRouters);
+
+app.use((req, res) => {
+  res.send("Página não encontrada!");
+});
 
 app.listen(process.env.PORT);
